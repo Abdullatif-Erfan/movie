bf9f9da3975411ee120c44f8d3e00a8e06023007
Abdul Latif Erfan <60866963+Abdullatif-Erfan@users.noreply.github.com>
2022-04-15 19:59:24 +0430

    Update store.tsx

--- client/src/redux/store.tsx
+++ client/src/redux/store.tsx
@@ -8,16 +8,3 @@ export const createStoreWithMiddleware = composeWithDevTools(
   applyMiddleware(...middlewares)
 )(createStore);
 export const store = createStoreWithMiddleware(RootReducer);
-
-// import { createStore, applyMiddleware } from "redux";
-// import { composeWithDevTools } from "redux-devtools-extension";
-// import thunk from "redux-thunk";
-// import rootReducer from "./reducers";
-
-// const initialState = {};
-// export const middleware = [thunk];
-// export const store = createStore(
-//   rootReducer,
-//   initialState,
-//   composeWithDevTools(applyMiddleware(...middleware))
-// );
